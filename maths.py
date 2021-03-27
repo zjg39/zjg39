c23ab6105fe74f293ac162cbea21520ccdea8e9a
zjg39 <81064068+zjg39@users.noreply.github.com>
2021-03-27 12:20:30 -0600

    Create maths.py

---
+++ maths.py
@@ -0,0 +1,25 @@
+# Basic math functions in Python 3
+
+import math
+
+def volume_sphere(r):
+  V = (4/3) * math.pi * (r**2)
+  print(V)
+  
+  
+volume_sphere(5)
+
+# Returns a number equal to the volume a sphere with radius (r)
+
+
+
+
+def area_triangle(b, h)
+  A = (1/2) * b * h
+  print(A)
+  
+
+area_triangle(5, 10)
+
+# Returns the area of the triangle.  In this case, 25.0
+
